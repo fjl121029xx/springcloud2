7e4e158c37b9083e0088d2a3255b19e884324451
fjl121029xx <sishengqikuo_xx@163.com>
2018-03-15 11:54:30 +0800

    spring cloud 作为消费端使用

---
+++ src/main/java/com/li/springcloud2/Springcloud2Application.java
@@ -0,0 +1,12 @@
+package com.li.springcloud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloud2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcloud2Application.class, args);
+	}
+}
